a6a71b87abb6d1af8722f97040266c036d7b7bf8
bohllmusic <p.viau@yahoo.fr>
2022-06-01 16:56:55 +0200

    finalisation captures d'ecran uml

--- REA3_site/Controller/publierController.php
+++ REA3_site/Controller/publierController.php
@@ -1,35 +1,36 @@
 <?php
 include '../View/publierView.php';
 include '../connexionBDD.php';
-include '../Model/class_publi.php';
+ include '../Model/class_publi.php'; 
 include '../Model/class_genre.php';
 
-if (isset($_POST ['texte']) && isset($_POST['lien']) && isset($_POST['genre']) && 
-$_POST['texte'] != ''&& $_POST['lien'] != ''&& $_POST['genre'] != ''){
-    $verifLien = @fopen($_POST['lien'], "r");//verification si le lien fonctionne, @fopen ouvre le lien,*
+if ( isset($_POST ['texte']) && isset($_POST['lien']) &&  isset($_POST['genre']) && 
+ $_POST['texte'] != ''&& $_POST['lien'] != ''&&  $_POST['genre'] != ''){
+     $verifLien = @fopen($_POST['lien'], "r"); //verification si le lien fonctionne, @fopen ouvre le lien,*
                                                 
-    if ($verifLien){        //si le lien est valide alors veriflien retourne true , alors on peut continuer
-        $publi = new Publication();
-        $genre = new Genre;
+    if ($verifLien){         //si le lien est valide alors veriflien retourne true , alors on peut continuer
+       $publi = new Publication();
+        
         $publi -> setTexte_publi(strip_tags(htmlspecialchars($_POST['texte'])));
-        $publi -> setLien_publi(strip_tags(htmlspecialchars($_POST['lien'])));
+        $publi -> setLien_musique_publi(strip_tags(htmlspecialchars($_POST['lien'])));
+        $genre = new Genre();
         $genre -> setNom_genre(strip_tags(htmlspecialchars($_POST['genre'])));
 
-        $genre -> createGenre($bdd);
+        $i=$genre -> createGenre($bdd);
 
         $genre -> readGenre($bdd);
         /* je verifie si le genre saisi est present dans la bdd , s'il l'est 
         je retourne l'id du genre , s'il ne l'est pas, j'insere le genre dans 
         la bdd et retourne l'id du nouveau genre*/
-/*         if (readGenre($bdd)){ //si la requete retourne qqch alors le genre est deja dans la bdd , 
+         if ($i!=NULL){ //si la requete retourne qqch alors le genre est deja dans la bdd , 
             return $retourGenre['id_genre_musique'];//je retourne simplement l'id du genre
         }else{ //j'enregistre le nouveau genre 
             $genre ->createGenre($bdd);
         }
- */
+ 
         
         
-    }else{echo 'le lien est invalide';}
-};
+   }else{echo 'le lien est invalide';}
+}
 
 ?>
